60e9d98e497492313c9f5cca214f875b7b7d2e54
Thomas Dreibholz <dreibh@simula.no>
2019-06-24 09:35:48 +0200

    Added SQL files and scripts.

---
+++ src/SQL/schema.sql
@@ -0,0 +1,53 @@
+-- STEP 2: Create Schema
+-- sudo -u postgres psql pingtraceroutedb <schema.sql
+--
+-- =================================================================
+--          #     #                 #     #
+--          ##    #   ####   #####  ##    #  ######   #####
+--          # #   #  #    #  #    # # #   #  #          #
+--          #  #  #  #    #  #    # #  #  #  #####      #
+--          #   # #  #    #  #####  #   # #  #          #
+--          #    ##  #    #  #   #  #    ##  #          #
+--          #     #   ####   #    # #     #  ######     #
+--
+--       ---   The NorNet Testbed for Multi-Homed Systems  ---
+--                       https://www.nntb.no
+-- =================================================================
+--
+-- High-Performance Connectivity Tracer (HiPerConTracer)
+-- Copyright (C) 2015-2019 by Thomas Dreibholz
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Contact: dreibh@simula.no
+
+
+-- ###### Experiment Schedule ###############################################
+DROP TABLE IF EXISTS ExperimentSchedule;
+CREATE TABLE ExperimentSchedule (
+   TimeStamp         TIMESTAMP        NOT NULL DEFAULT NOW(),
+
+   AgentHostIP       INET             NOT NULL,
+   AgentTrafficClass SMALLINT(0..255) NOT NULL DEFAULT 0,
+   AgentRouterIP     INET             NOT NULL,
+
+   MeasurementID     INTEGER          DEFAULT NULL,
+   ProbeID           INTEGER          DEFAULT NULL,
+   ProbeHostIP       INET             DEFAULT NULL,
+   ProbeRouterIP     INET             DEFAULT NULL,
+
+   State             SMALLINT         NOT NULL DEFAULT 0,
+
+   PRIMARY KEY (TimeStamp, AgentHostIP, AgentTrafficClass)
+);
---
+++ src/SQL/users.sql
@@ -0,0 +1,62 @@
+-- STEP 4: Create Users
+-- sudo -u postgres psql atlasmsndb <users.sql
+--
+-- =================================================================
+--          #     #                 #     #
+--          ##    #   ####   #####  ##    #  ######   #####
+--          # #   #  #    #  #    # # #   #  #          #
+--          #  #  #  #    #  #    # #  #  #  #####      #
+--          #   # #  #    #  #####  #   # #  #          #
+--          #    ##  #    #  #   #  #    ##  #          #
+--          #     #   ####   #    # #     #  ######     #
+--
+--       ---   The NorNet Testbed for Multi-Homed Systems  ---
+--                       https://www.nntb.no
+-- =================================================================
+--
+-- High-Performance Connectivity Tracer (HiPerConTracer)
+-- Copyright (C) 2015-2019 by Thomas Dreibholz
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Contact: dreibh@simula.no
+
+
+-- ##########################################################
+-- !!! IMPORTANT: Change the passwords before deployment! !!!
+-- ##########################################################
+
+
+REVOKE ALL ON DATABASE atlasmsndb FROM agent;
+REVOKE ALL ON Ping FROM agent;
+REVOKE ALL ON Traceroute FROM agent;
+DROP ROLE agent;
+CREATE ROLE agent WITH LOGIN ENCRYPTED PASSWORD '!agent!';
+GRANT CONNECT ON DATABASE atlasmsndb TO agent;
+GRANT SELECT ON TABLE ExperimentSchedule TO agent;
+GRANT UPDATE ON TABLE ExperimentSchedule TO agent;
+
+REVOKE ALL ON DATABASE atlasmsndb FROM scheduler;
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM scheduler;
+DROP ROLE scheduler;
+CREATE ROLE scheduler WITH LOGIN ENCRYPTED PASSWORD '!scheduler!';
+GRANT CONNECT ON DATABASE atlasmsndb TO scheduler;
+GRANT INSERT ON TABLE ExperimentSchedule TO scheduler;
+GRANT SELECT ON TABLE ExperimentSchedule TO scheduler;
+
+REVOKE ALL ON DATABASE atlasmsndb FROM maintainer;
+DROP ROLE maintainer;
+CREATE ROLE maintainer WITH LOGIN ENCRYPTED PASSWORD '!maintainer!';
+GRANT CONNECT ON DATABASE atlasmsndb TO maintainer;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO maintainer;
